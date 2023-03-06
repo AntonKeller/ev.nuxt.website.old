fa65a4745bbfd9bf400b4a2cb1749f5a92ace10e
Anton Keller <ToniWebDev@mail.ru>
2023-03-06 09:52:40 +0300

    main page fix font size / padding / margin

--- pages/index.vue
+++ pages/index.vue
@@ -2,49 +2,51 @@
   <div
     class="h-[calc(100vh-57px)] sm:h-[calc(100vh-77px)] md:h-screen background w-full text-red-50 overflow-y-scroll flex flex-col items-center md:pt-24">
 
-
     <div
-      class="px-4 text-4xl sm:text-6xl md:text-6xl lg:text-8xl duration-500 sm:pt-10 md:pt-14 lg:pt-24 flex flex-col items-center title">
-      <h1 class=" font-extrabold mt-2 sm:mt-4 md:mt-6 lg:mt-8 tracking-wide">Everest
-        consulting</h1>
+      class="text-2xl sm:text-4xl md:text-5xl lg:text-6xl duration-500 sm:pt-10 md:pt-14 lg:pt-24 flex flex-col items-center sm:items-start title">
+      <h1 class=" font-extrabold mt-2 sm:mt-4 md:mt-6 lg:mt-8 sm:tracking-wide">
+        Everest consulting
+      </h1>
       <h1
-        class=" font-extrabold mt-2 sm:mt-4 md:mt-6 lg:mt-8 sm:translate-x-20 lg:translate-x-44 tracking-wide">
+        class=" font-extrabold mt-2 sm:mt-4 md:mt-6 lg:mt-8 sm:tracking-wide">
         лидер оценки
       </h1>
       <div
-        class="rounded-md w-full border-b-4 md:border-b-8 border-b-red-700 mt-2 sm:mt-4 md:mt-6 lg:mt-8 sm:-translate-x-10 lg:-translate-x-10"></div>
+        class="rounded-md w-full border-b-4 md:border-b-8 border-b-red-700 mt-2 sm:mt-4 md:mt-6 lg:mt-8"></div>
     </div>
 
 
     <div
       class="px-4 w-full sm:w-11/12 md:w-10/12 lg:w-9/12 xl:w-8/12 sm:pt-10 md:pt-14 lg:pt-24 flex flex-col items-center sm:flex-row sm:items-end">
-      <img class="mt-4 sm:mt-0 object-cover object-fill w-24 sm:w-32 md:w-48 duration-500"
+      <img class="icon-animate mt-4 sm:mt-0 object-cover object-fill w-20 sm:w-24 md:w-32 duration-500"
            src="../assets/stickers/company-sticker-red-50.png"
            alt="not found sticker company">
-      <div class="flex flex-col items-center sm:items-start md:items-end font-extrabold mt-4">
+      <div class="show-from-top flex flex-col items-center sm:items-start md:items-end font-extrabold mt-4">
         <h2
-          class="text-center sm:text-left md:text-right lg:mt-6 text-2xl sm:text-4xl md:text-4xl lg:text-6xl duration-500">
-          Немного о нашей компании...
+          class="text-center sm:text-left md:text-right lg:mt-4 text-xl sm:text-2xl md:text-3xl lg:text-4xl duration-500">
+          Наша компания
         </h2>
         <p
-          class="mt-4 lg:mt-6 text-xl sm:text-2xl md:text-2xl lg:text-4xl text-center sm:text-left md:text-right duration-500">
+          class="mt-2 lg:mt-4 text-lg sm:text-xl md:text-2xl lg:text-3xl text-center sm:text-left md:text-right duration-500">
           Компания Эверест Консалтинг создана в 2010 году. Учредители компании ранее занимали руководящие позиции в
           группе оценки российского подразделения международной аудиторской компании
           <span
-            class="z-50 bg-red-700 px-4 font-extrabold tracking-wider inline-block md:translate-x-16 duration-500 rounded-sm">«PricewaterhouseCoopers».</span>
+            class="z-50 bg-red-700 px-4 font-extrabold tracking-wider inline-block duration-500 rounded-sm">
+            «PricewaterhouseCoopers».
+          </span>
         </p>
       </div>
     </div>
 
 
     <div
-      class="bg-red-filler z-10 px-4 mt-2 md:mt-0 text-gray-700 flex flex-col md:flex-row items-center justify-center relative py-4 sm:py-6 md:py-16 px-4 sm:px-8 md:px-16 lg:px-28">
+      class="bg-red-filler z-10 px-4 mt-2 md:mt-0 text-gray-700 flex flex-col md:flex-row items-center justify-center relative py-2 sm:py-6 md:py-16 px-4 sm:px-8 md:px-16 lg:px-28">
       <div
         class="w-full md:w-11/12 xl:w-10/12 z-30 min-h-56 flex flex-col md:flex-row items-center">
-        <img class="show-from-left object-cover object-fill w-32 md:w-48 duration-500"
+        <img class="show-from-left object-cover object-fill w-24 sm:w-32 md:w-48 duration-500"
              src="../assets/stickers/target-sticker-gray-shadow.png"
              alt="not found sticker target">
-        <p class="show-from-right text-xl sm:text-2xl md:text-2xl lg:text-2xl text-center font-extrabold duration-500">
+        <p class="text-lg sm:text-xl md:text-xl lg:text-2xl text-center font-extrabold duration-500">
           <span class="uppercase">Наша цель</span> – оказание полного комплекса консультационных услуг в области
           оценки и консалтинга (финансового,
           управленческого, налогового и строительного) специалистами, обладающими международными квалификациями и
@@ -54,8 +56,8 @@
     </div>
 
 
-    <div class="mt-2 sm:mt-4 md:mt-10 lg:mt-16 px-4 sm:px-32 flex flex-col items-center w-12/12 sm:w-6/12">
-      <h2 class="font-extrabold text-2xl sm:text-4xl md:text-4xl lg:text-6xl text-center">
+    <div class="mt-2 sm:mt-4 md:mt-6 lg:mt-8 px-4 sm:px-32 flex flex-col items-center w-12/12 sm:w-6/12">
+      <h2 class="font-extrabold text-xl sm:text-2xl md:text-3xl lg:text-4xl text-center">
         Наши преимущества
       </h2>
       <div class="bg-red-100 mt-2 hidden md:block p-1.5 w-full rounded-sm"></div>
@@ -63,9 +65,9 @@
 
 
     <div
-      class="flex flex-col lg:flex-row items-center lg:items-start justify-center mt-4 sm:mt-8 lg:mt-16 w-full px-4 sm:px-8 duration-500">
+      class="flex flex-col lg:flex-row items-center lg:items-start justify-center mt-0 sm:mt-4 lg:mt-8 w-full px-4 sm:px-8 duration-500">
       <div
-        class="bg-white-filler mt-4 content-animate rounded-md border hover:-translate-y-10 duration-500 h-full border-gray-700 m-2 p-2 backdrop-blur-sm backdrop-brightness-75 sm:backdrop-brightness-100 w-full sm:w-10/12 lg:w-7/12 xl:w-6/12"
+        class="bg-white-filler mt-2 content-animate rounded-md border hover:-translate-y-10 duration-500 h-full border-gray-700 m-2 p-2 backdrop-blur-sm backdrop-brightness-75 sm:backdrop-brightness-100 w-full sm:w-10/12 lg:w-7/12 xl:w-6/12"
         v-for="el of mainContent"
         :style="{animationDuration: 0.5 + el.id / 6 + 's'}">
 
@@ -81,8 +83,8 @@
     </div>
 
 
-    <div class="mt-2 sm:py-4 md:py-10 lg:py-16 px-4 sm:px-32 flex flex-col items-center w-12/12 sm:w-6/12">
-      <h2 class="font-extrabold text-2xl sm:text-4xl md:text-4xl lg:text-6xl text-center">
+    <div class="mt-2 sm:py-2 md:py-8 lg:py-8 px-4 sm:px-32 flex flex-col items-center w-12/12 sm:w-6/12">
+      <h2 class="font-extrabold text-xl sm:text-2xl md:text-3xl lg:text-4xl text-center">
         Руководство компании
       </h2>
       <div class="bg-red-100 mt-2 hidden md:block p-1.5 w-full rounded-sm"></div>
@@ -90,21 +92,21 @@
 
 
     <div
-      class="mt-2 mb-4 sm:mb-10 px-4 sm:px-20 md:px-6 w-full text-gray-300 flex flex-col justify-start md:flex-row duration-500">
+      class="mt-2 mb-4 sm:mb-8 px-4 sm:px-20 md:px-6 w-full text-gray-300 flex flex-col justify-start md:flex-row duration-500">
 
       <div class="mt-2 md:mt-0">
         <img class="object-fill object-cover rounded-sm" src="../assets/avatars/avatar-1.webp" alt="no image">
         <h3
-          class="shadow-gray-800 shadow-2xl font-extrabold px-2 py-2 md:py-4 rounded-sm text-2xl lg:text-2xl text-center bg-red-700">
+          class="shadow-gray-800 shadow-2xl font-extrabold px-2 py-2 md:py-4 rounded-sm text-xl lg:text-2xl text-center bg-red-700">
           <span class="block py-1 my-1 text-lg lg:text-xl bg-red-800 rounded-sm">Директор</span>
           Иванов Генадий Викторович
         </h3>
       </div>
 
-      <div class="mt-2 md:mt-0 md:-translate-y-8 md:mx-4">
+      <div class="mt-2 md:mt-0 md:mx-4">
         <img class="object-fill object-cover rounded-sm" src="../assets/avatars/avatar-2.jpg" alt="no image">
         <h3
-          class="shadow-gray-800 shadow-2xl font-extrabold px-2 py-2 md:py-4 rounded-sm text-2xl lg:text-2xl text-center bg-red-700">
+          class="shadow-gray-800 shadow-2xl font-extrabold px-2 py-2 md:py-4 rounded-sm text-xl lg:text-2xl text-center bg-red-700">
           <span class="block py-1 my-1 text-lg lg:text-xl bg-red-800 rounded-sm">Генеральный директор</span>
           Иванов Генадий Викторович
         </h3>
@@ -113,7 +115,7 @@
       <div class="mt-2 md:mt-0">
         <img class="object-fill object-cover rounded-sm" src="../assets/avatars/avatar-3.jpg" alt="no image">
         <h3
-          class="shadow-gray-800 shadow-2xl font-extrabold px-2 py-2 md:py-4 rounded-sm text-2xl lg:text-2xl text-center bg-red-700">
+          class="shadow-gray-800 shadow-2xl font-extrabold px-2 py-2 md:py-4 rounded-sm text-xl lg:text-2xl text-center bg-red-700">
           <span class="block py-1 my-1 text-lg lg:text-xl bg-red-800 rounded-sm">Директор</span>
           Иванов Генадий Викторович
         </h3>
@@ -187,14 +189,14 @@ export default {
   }
 }
 
-.show-from-right {
-  animation: animate-from-right ease 0.8s;
+.show-from-top {
+  animation: animate-from-top ease 0.8s;
 }
 
-@keyframes animate-from-right {
+@keyframes animate-from-top {
   from {
     opacity: 0;
-    transform: translateX(100vw);
+    transform: translateY(-50px);
   }
 }
 
@@ -212,6 +214,17 @@ export default {
 
 .title {
   animation: show-title-animate ease 0.8s;
+}
+
+.icon-animate{
+  animation: show-icon-animate ease 0.8s;
+}
+
+@keyframes show-icon-animate {
+  from {
+    opacity: 0;
+    transform: scale(0);
+  }
 }
 
 @keyframes show-title-animate {
